367bef5e7fb14ca1d5feb37e5110ef7639340032
Shitdogg <pratchaya.ueapisitkul@gmail.com>
2025-05-11 02:39:30 +0700

    fix: export default module

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 // index.ts
 import SequentialGenerator from "./SequentialGenerator"; // import from SequentialGenerator.ts (หรือ .js ถ้าเป็น JavaScript)
 
-export { SequentialGenerator };
+export default SequentialGenerator;
